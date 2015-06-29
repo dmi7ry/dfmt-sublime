4c177b24bddef925579147c99491178cad16216c
Dmi7ry <dmitry@indiedev.ru>
2015-06-29 18:46:47 +0600

    dfmt result checking

--- dformat.py
+++ dformat.py
@@ -5,26 +5,25 @@ plugin_settings = None
 def read_settings(key, default):
     global plugin_settings
     if plugin_settings is None:
-        plugin_settings = sublime.load_settings('dformat.sublime-settings')
+        plugin_settings = sublime.load_settings("dformat.sublime-settings")
 
     return sublime.active_window().active_view().settings().get(key, plugin_settings.get(key, default))
 
 class dformat(sublime_plugin.TextCommand):
 
     def run(self, edit):
-        # get windows temporary directory
-
+        # get temporary directory
         tmpdir = tempfile.mkdtemp("", "_dfmt_temp_")
-        name = tmpdir + '\\sublime_dfmt_tmp.d'
+        name = tmpdir + "\\sublime_dfmt_tmp.d"
 
         # check config file
-        config_path = read_settings('config_path', '')
+        config_path = read_settings("config_path", "")
         if os.path.isfile(config_path):
             # print "Config path: " + config_path
             try:
                 shutil.copy(config_path, tmpdir)
             except IOError:
-                # print 'Can\'t copy config file to ' + path
+                sublime.status_message("Can't copy config file to " + path)
                 pass
 
         # print "[DFormat] Save text to file: " + name
@@ -34,22 +33,28 @@ class dformat(sublime_plugin.TextCommand):
         text = self.view.substr(all_text)
 
         # save text
-        f = open(name, 'w')
+        f = open(name, "w")
         f.write(text)
         f.close()
 
         # run dfmt
-        os.system(r'dfmt --inplace ' + f.name)
+        res = os.system(r"dfmt --inplace " + f.name)
+        if res == 0:
+            # clear view
+            self.view.erase(edit, all_text)
 
-        # clear view
-        self.view.erase(edit, all_text)
+            #read new text
+            f = open(name)
+            formated = f.read()
+            f.close()
 
-        #read new text
-        f = open(name)
-        formated = f.read()
-        f.close()
+            self.view.insert(edit, 0, formated)
 
-        self.view.insert(edit, 0, formated)
+            msg = "[DFormat] Formating finished"
+        else:
+            msg = "[DFormat] dfmt error (dfmt.exe not found?)"
 
         # remove temporary file and dir
         shutil.rmtree(tmpdir)
+
+        sublime.status_message(msg)
